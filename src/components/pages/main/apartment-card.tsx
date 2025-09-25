e1c9ba73ef0087b7975a270c24d033f58da6a064
vVladEr <vlader.2004.ekb@gmail.com>
2025-09-25 22:42:00 +0500

    fixes and clean up

--- src/components/pages/main/apartment-card.tsx
+++ src/components/pages/main/apartment-card.tsx
@@ -14,7 +14,7 @@ function OfferCard(card : CardInfo) : JSX.Element {
                     <div className="place-card__info">
                         <div className="place-card__price-wrapper">
                         <div className="place-card__price">
-                            <b className="place-card__price-value">&euro;{card.priceInEuros} 120</b>
+                            <b className="place-card__price-value">&euro;{card.priceInEuros}</b>
                             <span className="place-card__price-text">&#47;&nbsp;night</span>
                         </div>
                         <button className="place-card__bookmark-button button" type="button">
--- src/components/pages/main/main.tsx
+++ src/components/pages/main/main.tsx
@@ -110,54 +110,11 @@ function MainPage({cardsAmount} : MainPageProps) : JSX.Element {
                         Array<JSX.Element>(cardsAmount).fill(OfferCard({
                             priceInEuros: 120, 
                             imageName: "apartment-01.jpg",
-                            description: 'Beautiful &amp; luxurious apartment at great location',
+                            description: 'Beautiful & luxurious apartment at great location',
                             cardType: CardType.Apartment
                         }
                         ))
                     }
-                    <OfferCard 
-                        priceInEuros={120} 
-                        imageName="apartment-01.jpg"
-                        description = 'Beautiful &amp; luxurious apartment at great location'
-                        cardType={CardType.Apartment}
-                    />
-                    <article className="cities__card place-card">
-                    <div className="place-card__mark">
-                        <span>Premium</span>
-                    </div>
-                    <div className="cities__image-wrapper place-card__image-wrapper">
-                        <a href="#">
-                        <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-                        </a>
-                    </div>
-                    <div className="place-card__info">
-                        <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                            <b className="place-card__price-value">&euro;120</b>
-                            <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button button" type="button">
-                            <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                            </svg>
-                            <span className="visually-hidden">To bookmarks</span>
-                        </button>
-                        </div>
-                        <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                            <span style={{
-                                width: "80%"
-                            }}/>
-                            <span className="visually-hidden">Rating</span>
-                        </div>
-                        </div>
-                        <h2 className="place-card__name">
-                        <a href="#">Beautiful &amp; luxurious apartment at great location</a>
-                        </h2>
-                        <p className="place-card__type">Apartment</p>
-                    </div>
-                    </article>
-
                     <article className="cities__card place-card">
                     <div className="cities__image-wrapper place-card__image-wrapper">
                         <a href="#">
