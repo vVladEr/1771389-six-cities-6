301b90616433503f35ca5221adea90200e2831e5
vVladEr <vlader.2004.ekb@gmail.com>
2025-11-09 13:04:19 +0500

    fix linting

--- src/pages/offer/review-form.tsx
+++ src/pages/offer/review-form.tsx
@@ -35,7 +35,9 @@ export function ReviewForm() : JSX.Element{
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled={reviewFormData.rating === 0
-          || reviewFormData.review.length < 50}>Submit</button>
+          || reviewFormData.review.length < 50}
+        >Submit
+        </button>
       </div>
     </form>
   );
