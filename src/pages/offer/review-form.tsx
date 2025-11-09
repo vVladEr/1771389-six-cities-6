5b480c7b8fe747fe5f3a4fabecb946b731821c5f
vVladEr <vlader.2004.ekb@gmail.com>
2025-11-09 13:00:24 +0500

    fix review form submit button

--- src/pages/offer/review-form.tsx
+++ src/pages/offer/review-form.tsx
@@ -34,7 +34,8 @@ export function ReviewForm() : JSX.Element{
         <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={reviewFormData.rating === 0
+          || reviewFormData.review.length < 50}>Submit</button>
       </div>
     </form>
   );
