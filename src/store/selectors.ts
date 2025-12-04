3a5c4f5ebb35a788195e5849f9740b2757b11d8e
vVladEr <vlader.2004.ekb@gmail.com>
2025-12-04 21:45:40 +0500

    Private route now use store

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -5,6 +5,7 @@ export const curCity = (state: OffersState) => state.city;
 export const allCities = (state: OffersState) => state.cities;
 export const allPlaces = (state: OffersState) => state.places;
 export const isLoadingOffers = (state: OffersState) => state.isLoadingOffers;
+export const authStatus = (state: OffersState) => state.authorizationStatus;
 
 export const offersByCity = createSelector([allPlaces, curCity],
   (offers, city) => offers.filter((offer) => offer.city.name === city.name));
