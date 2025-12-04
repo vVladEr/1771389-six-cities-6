e3496dcd0f3376862b2efc3f617ec07c8e1a81aa
vVladEr <vlader.2004.ekb@gmail.com>
2025-12-04 21:57:35 +0500

    add sign in button in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useSelector } from 'react-redux';
+import { authStatus } from '../../store/selectors';
 
 export function Header() : JSX.Element{
+  const authorizationStatus = useSelector(authStatus)
   return(
     <header className="header">
       <div className="container">
@@ -13,19 +16,28 @@ export function Header() : JSX.Element{
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </Link>
-              </li>
-              <li className="header__nav-item">
-                <Link className="header__nav-link" to={AppRoute.Login}>
-                  <span className="header__signout">Sign out</span>
-                </Link>
-              </li>
+              {
+                authorizationStatus === AuthorizationStatus.Auth ?
+                    <><li className="header__nav-item user">
+                    <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      <span className="header__favorite-count">3</span>
+                    </Link>
+                  </li><li className="header__nav-item">
+                      <Link className="header__nav-link" to={AppRoute.Login}>
+                        <span className="header__signout">Sign out</span>
+                      </Link>
+                    </li></>
+                :
+                    <li className="header__nav-item">
+                      <Link className="header__nav-link" to={AppRoute.Login}>
+                        <span className="header__signout">Sign In</span>
+                      </Link>
+                  </li>
+              }
+
             </ul>
           </nav>
         </div>
