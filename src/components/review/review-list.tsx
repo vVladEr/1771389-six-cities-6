0e7735be7859105ad2881b4b602b19b95fc0e9fb
vVladEr <vlader.2004.ekb@gmail.com>
2025-12-14 22:24:29 +0500

    fix reviews number

--- src/components/review/review-list.tsx
+++ src/components/review/review-list.tsx
@@ -11,7 +11,7 @@ export function ReviewsList({offerId}: ReviewsListProps): JSX.Element{
   const reviews: Reviews = [];
   return(
     <React.Fragment>
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {
           reviews.map(review =>
